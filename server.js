75b5a4389019f521e9a17d75f794c7811d1f8230
fishmick <68821668+fishmick@users.noreply.github.com>
2025-12-11 12:41:26 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+
+const nodemailer = require("nodemailer");
+
+const PORT = 3000;
+
+// Middleware
+
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+   
+
+})
+
+app.post('/', (req, res) => {
+    
+
+    const transporter = nodemailer.createTransport({
+        service:"gmail",
+		auth: {
+			user: 'fishmick.callahan@gmail.com',	// My email address
+			pass: 'lurxnbzihsjkhmmt'
+	}
+
+
+})
+    const mailOptions = {
+        from: req.body.email,
+        to: 'fishmick.callahan@gmail.com',
+        subject: `Message from ${req.body.name}: ${req.body.subject}}`,
+        text: req.body.message
+    }
+
+   transporter.sendMail(mailOptions, (error, info) => {
+	if (error) {
+		console.log(error);
+        res.send('error');
+	} else {
+		console.log("Email Sent: " + info.response);
+        res.send('success');
+		}
+		
+	})})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
